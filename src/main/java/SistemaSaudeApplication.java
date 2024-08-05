f386e6ad38d7c351e7ee48e2de23ef99cd599a2d
Heloise <heloiseapereira@gmail.com>
2024-08-05 16:50:30 -0300

    logger

--- src/main/java/SistemaSaudeApplication.java
+++ src/main/java/SistemaSaudeApplication.java
@@ -1,13 +1,20 @@
-package lab._5.SistemaSaude;
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SistemaSaudeApplication {
 
+	private static Logger logger = LogManager.getLogger(SistemaSaudeApplication.class);
+
 	public static void main(String[] args) {
+
 		SpringApplication.run(SistemaSaudeApplication.class, args);
+		logger.debug("Debug msg");
+		logger.info("Info msg");
+		logger.error("Error msg");
 	}
+
 
 }
